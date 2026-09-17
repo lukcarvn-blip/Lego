ea28ebd72e801b2c84b93e41cde975121d652dd9
Ten Cua Ban <lukcarvn@gmail.com>
2026-09-17 13:01:32 +0700

    style: refine lego character silhouette svg on request cards

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -861,14 +861,28 @@ export const Home = () => {
                     {/* Lego Silhouette */}
                     <div style={{ position: 'absolute', bottom: '3rem', left: '50%', transform: 'translateX(-50%)', width: '80%', height: '66%', zIndex: 1, opacity: 0.15 }}>
                       <svg viewBox="0 0 100 150" xmlns="http://www.w3.org/2000/svg" style={{ width: '100%', height: '100%' }}>
-                        <rect x="35" y="10" width="30" height="25" rx="5" fill="#fff" />
-                        <rect x="42" y="5" width="16" height="5" rx="2" fill="#fff" />
-                        <path d="M 25 40 L 75 40 L 85 90 L 15 90 Z" fill="#fff" />
-                        <rect x="20" y="92" width="25" height="50" rx="3" fill="#fff" />
-                        <rect x="55" y="92" width="25" height="50" rx="3" fill="#fff" />
-                        <path d="M 20 45 Q 5 60 10 80" stroke="#fff" strokeWidth="12" strokeLinecap="round" fill="none" />
-                        <path d="M 80 45 Q 95 60 90 80" stroke="#fff" strokeWidth="12" strokeLinecap="round" fill="none" />
-                        <text x="50" y="70" fill="var(--color-accent)" fontSize="50" fontWeight="bold" textAnchor="middle" dominantBaseline="middle">?</text>
+                        {/* Stud */}
+                        <rect x="40" y="8" width="20" height="6" rx="2" fill="#fff" />
+                        {/* Head */}
+                        <rect x="30" y="14" width="40" height="32" rx="8" fill="#fff" />
+                        {/* Neck */}
+                        <rect x="42" y="46" width="16" height="4" fill="#fff" />
+                        {/* Torso */}
+                        <path d="M 34 50 L 66 50 L 76 95 L 24 95 Z" fill="#fff" />
+                        {/* Hips */}
+                        <path d="M 24 97 L 76 97 L 76 108 L 24 108 Z" fill="#fff" />
+                        {/* Legs */}
+                        <rect x="24" y="110" width="23" height="35" rx="3" fill="#fff" />
+                        <rect x="53" y="110" width="23" height="35" rx="3" fill="#fff" />
+                        {/* Arms */}
+                        <path d="M 32 50 C 15 50 10 70 12 85 C 13 90 20 90 24 85 C 24 75 22 65 32 60 Z" fill="#fff" />
+                        <path d="M 68 50 C 85 50 90 70 88 85 C 87 90 80 90 76 85 C 76 75 78 65 68 60 Z" fill="#fff" />
+                        {/* Hands */}
+                        <path d="M 16 82 C 6 82 4 98 14 98 C 22 98 24 90 18 88 C 16 87 14 92 10 90 C 8 88 8 85 10 84 C 14 82 16 86 18 86 C 22 84 20 82 16 82 Z" fill="#fff" />
+                        <path d="M 84 82 C 94 82 96 98 86 98 C 78 98 76 90 82 88 C 84 87 86 92 90 90 C 92 88 92 85 90 84 C 86 82 84 86 82 86 C 78 84 80 82 84 82 Z" fill="#fff" />
+                        
+                        {/* Question mark */}
+                        <text x="50" y="75" fill="var(--color-accent)" fontSize="42" fontWeight="900" textAnchor="middle" dominantBaseline="middle">?</text>
                       </svg>
                     </div>
 
